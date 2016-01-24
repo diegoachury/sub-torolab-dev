385e84d19ab411d79855fcffb77985a72038545d
diegoachury <tiran_o@hotmail.com>
2016-01-24 11:09:45 -0500

    iniciando repo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name subwaytorolabsdevApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the subwaytorolabsdevApp
+ */
+angular.module('subwaytorolabsdevApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
